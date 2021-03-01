171e7bf8b765a15cd1d82fcaf342e2e6943b0810
svetlana <developer-svetlana@yandex.ru>
2021-03-01 14:30:20 +0500

    add all dz1

--- server.js
+++ server.js
@@ -2,24 +2,29 @@ const http = require('http');
 const fs = require('fs');
 
 const server = http.createServer(function (req, res) {
-    console.log(req.url)
+  console.log(req.url)
 
-   let body = null
-  
-
-    try {
-        body = fs.readFileSync(`./public${req.url}`)
+  let body = null
+  try {
+    // /img/user.svg
+    const ext = req.url.split('.')[1]
+    console.log(ext);
+    const isSvg = ext === 'svg';
+    if (isSvg) {
+      res.setHeader('Content-Type', 'image/svg+xml');
     }
-    catch (err) {
-        body = fs.readFileSync(`./public/index.html`)
-    }
 
-   
-    res.end(body)
+    body = fs.readFileSync(`./public${req.url}`)
+  } catch (err) {
+    body = fs.readFileSync(`./public/index.html`)
+  }
+
+  res.end(body)
 });
 
-
 const port = process.env.PORT || 3000;
+
 server.listen(port);
 
-console.log(`Server started on port ${port}!`);
+console.log(`Server started on port ${port}!`); 
+
